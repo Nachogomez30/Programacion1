c5b5234200234121a7350a277afa91fc4cb5b828
Ignacio Gomez <ignaciogomezgarcia27@gmail.com>
2023-10-16 18:29:57 -0300

    Parcial Nº1

---
+++ TPNº2 - Ignacio Gomez - Comision A.py
@@ -0,0 +1,15 @@
+"""# TRBAJO PRACTICO Nº2:
+
+#Ejercicio 1:
+lenght = float(input("Ingrese la base del rectangulo: "))
+width = float(input("Ingrese la altura del rectangulo: "))
+area = lenght*width
+perimeter = lenght*2+width*2
+print("El area del rectangulo es: "+area)
+print("El perimetro del rectangulo es: "+width)
+"""
+while True:
+    codigo=input("Ingrese una palabra de 5 letras: ")
+    if len(codigo)==5:
+        break
+print() 
