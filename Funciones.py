9a0a19fa4cb1c09906ef7a533a13acd1db0dd21d
Ignacio Gomez <ignaciogomezgarcia27@gmail.com>
2023-10-30 21:58:46 -0300

    Funciones

--- Funciones.py
+++ Funciones.py
@@ -43,3 +43,69 @@ import os
 def clear_terminal():
     os.system('clear' if os.name == 'posix' else 'cls')
 
+
+# FUNCIONES DE TPNº5:
+def validation_dni(dni):
+    if len(str(dni)) >=7 and len(str(dni)) <=8:
+        return True
+    else:
+        return False
+
+def length_word(spaceses):
+        spaceses=spaceses.strip()
+        words=spaceses.split()
+        if words:
+            last_word=words[-1]
+            return len(last_word)
+        else:
+            return 0
+
+def id_club(name,dni):
+    words = name.split()
+    first_name = words[0]
+    last_name = words[-1]
+    three_digitd = dni[:3]
+    identifier = first_name+str(len(last_name))+three_digitd
+    return identifier
+
+# FUNCIONES TPNº7
+def bubble_sort(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+def selection_sort(list, key):
+    n = len(list)
+    for i in range(n):
+        idx = i
+        for j in range(i + 1, n):
+            if list[j][key] < list[idx][key]:
+                idx = j
+        list[i], list[idx] = list[idx], lit[i]
+
+# FUNCIONES TPNº8
+def count_digits(n):
+    if n < 10:
+        return 1
+    else:
+        return 1 + count_digits(n // 10)
+
+def is_power_of(n, b):
+    if n == 1:
+        return True
+    if n % b != 0 or n == 0:
+        return False
+    return is_power_of(n / b, b)
+
+def positions_of(a, b):
+    def find_positions(text, substring, index):
+        if index + len(substring) > len(text):
+            return []
+        elif text[index:index + len(substring)] == substring:
+            return [index] + find_positions(text, substring, index + 1)
+        else:
+            return find_positions(text, substring, index + 1)
+    
+    return find_positions(a, b, 0)
